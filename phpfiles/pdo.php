d5a0977ab2e8c6c84682cf652db90f59e279afb9
joanPiquero <piquerojoan@gmail.com>
2019-03-16 12:00:04 +0100

    init repo

---
+++ phpfiles/pdo.php
@@ -0,0 +1,9 @@
+<?php
+$pdo= new PDO('mysql:hostlocalhost;port=8809;dbname=Conferencia','joan','joan');
+
+// See the "errors" folder for details...
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+?>
+
