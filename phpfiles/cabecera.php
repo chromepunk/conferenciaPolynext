7c7b4b1b034a2940896c327f282a533a43c9c669
Joan Piquero cilla <joanpiquerocilla@MacBook-Pro-de-Joan.local>
2019-04-08 10:33:17 +0200

    separacion en cabecera y pie

---
+++ phpfiles/cabecera.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="../css/stilo1.css">
+</head>
+<body>
+
+<div class="header">
+	<h1>polyNext</h1>
+</div>
+
+
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="javascript:alert('hola')">Conferencias</a></li>
+  <li><a href="#contact">Posters</a></li>
+  <li class="right"><a href="#about">Login</a></li>
+</ul>
--- phpfiles/index.php
+++ phpfiles/index.php
@@ -1,22 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="../css/stilo1.css">
-</head>
-<body>
-
-<div class="header">
-	<h1>polyNext</h1>
-</div>
-
-
-<ul>
-  <li><a class="active" href="#home">Home</a></li>
-  <li><a href="">Conferencias</a></li>
-  <li><a href="#contact">Posters</a></li>
-  <li class="right"><a href="#about">Login</a></li>
-</ul>
+<?php
+	include "cabecera.php";
+?>
 
 <div style="padding:0 16px;">
   <h2>Responsive Topnav Example</h2>
@@ -24,6 +8,6 @@
   <p>You will learn more about media queries and responsive web design later in our CSS Tutorial.</p>
   <h4>Resize the browser window to see the effect.</h4>
 </div>
-
-</body>
-</html>
+<?php 
+	include "pie.php";
+?>
